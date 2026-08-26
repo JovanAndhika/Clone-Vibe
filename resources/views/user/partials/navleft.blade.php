5a8d4720d77a0dd3f24d8faec671342600085695
Jovan Andhika <c14220013@john.petra.ac.id>
2026-08-26 15:03:00 +0700

    fix: nama library menjadi playlist

--- resources/views/user/partials/navleft.blade.php
+++ resources/views/user/partials/navleft.blade.php
@@ -1,7 +1,8 @@
 <div class="col-3 p-0 m-0 fontMonsseratExtraBold d-flex align-items-center justify-content-center resizable">
     <ul class="nav flex-column text-white hoverable">
         <li class="nav-item d-inline-flex align-items-center my-3">
-            <a class="nav-link text-white @if ($active == 'nowPlaying') active @endif" href="{{ route('user.nowPlaying') }}#jumphere"><i
+            <a class="nav-link text-white @if ($active == 'nowPlaying') active @endif"
+                href="{{ route('user.nowPlaying') }}#jumphere"><i
                     class="bi bi-play-circle text-white @if ($active == 'nowPlaying') active @endif"></i></a>
             <span>
                 <a class="nav-link text-white @if ($active == 'nowPlaying') active @endif"
@@ -10,7 +11,8 @@
         </li>
 
         <li class="nav-item d-inline-flex align-items-center my-3">
-            <a class="nav-link text-white @if ($active == 'home') active @endif" href="{{ route('user.index') }}#jumphere"><i
+            <a class="nav-link text-white @if ($active == 'home') active @endif"
+                href="{{ route('user.index') }}#jumphere"><i
                     class="bi bi bi-house text-white @if ($active == 'home') active @endif"></i></a>
             <span>
                 <a class="nav-link text-white @if ($active == 'home') active @endif"
@@ -19,7 +21,8 @@
         </li>
 
         <li class="nav-item d-inline-flex align-items-center my-3">
-            <a class="nav-link text-white @if ($active == 'search') active @endif" href="{{ route('user.search') }}#jumphere"><i
+            <a class="nav-link text-white @if ($active == 'search') active @endif"
+                href="{{ route('user.search') }}#jumphere"><i
                     class="bi bi-search text-white @if ($active == 'search') active @endif"></i></a>
             <span>
                 <a class="nav-link text-white @if ($active == 'search') active @endif"
@@ -28,25 +31,28 @@
         </li>
 
         <li class="nav-item d-inline-flex align-items-center my-3">
-            <a class="nav-link text-white @if ($active == 'history') active @endif" href="{{ route('user.history') }}#jumphere"><i
+            <a class="nav-link text-white @if ($active == 'history') active @endif"
+                href="{{ route('user.history') }}#jumphere"><i
                     class="bi bi-clock-history text-white @if ($active == 'history') active @endif"></i></a>
             <span>
                 <a class="nav-link text-white @if ($active == 'history') active @endif"
                     href="{{ route('user.history') }}#jumphere">History</a>
             </span>
         </li>
-        <li class="nav-item d-inline-flex align-items-center my-3">
-            <a class="nav-link text-white @if ($active == 'library') active @endif" href="{{ url('/user/playlists') }}#jumphere"><i
-                    class="bi bi-collection-play text-white @if ($active == 'library') active @endif"></i></a>
-            <span>
-                <a class="nav-link text-white @if ($active == 'library') active @endif"
-                    href="{{ url('/user/playlists') }}#jumphere">Library</a>
-            </span>
 
+        <li class="nav-item d-inline-flex align-items-center my-3">
+            <a class="nav-link text-white @if ($active == 'playlist') active @endif"
+                href="{{ url('/user/playlists') }}#jumphere"><i
+                    class="bi bi-collection-play text-white @if ($active == 'playlist') active @endif"></i></a>
+            <span>
+                <a class="nav-link text-white @if ($active == 'playlist') active @endif"
+                    href="{{ url('/user/playlists') }}#jumphere">Playlist</a>
+            </span>
         </li>
 
         <li class="nav-item d-inline-flex align-items-center my-3">
-            <a class="nav-link text-white @if ($active == 'like') active @endif" href="{{ route('user.like') }}#jumphere"><i
+            <a class="nav-link text-white @if ($active == 'like') active @endif"
+                href="{{ route('user.like') }}#jumphere"><i
                     class="bi bi-heart text-white @if ($active == 'like') active @endif"></i></a>
             <span>
                 <a class="nav-link text-white @if ($active == 'like') active @endif"
